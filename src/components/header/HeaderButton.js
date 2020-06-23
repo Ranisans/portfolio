3e084c6577994897a44a2c6e51785e8a0a432d56
Ranisans <ranisans@yahoo.com>
2020-06-23 11:01:31 +0300

    feat: add correct animation transition

--- src/components/header/HeaderButton.js
+++ src/components/header/HeaderButton.js
@@ -2,21 +2,30 @@ import React, { useState, createRef } from 'react';
 
 const HeaderButton = ({ callback }) => {
   const [isClicked, setIsClicked] = useState(false);
-
+  const animationDuration = 500;
   const ref = createRef();
 
   const onClick = () => {
+    const { classList } = ref.current;
     if (isClicked) {
-      ref.current.classList.remove('header_button_cross');
+      classList.remove('header_button_cross-active');
+      classList.remove('header_button_cross');
+      setTimeout(() => {
+        classList.add('header_button-active');
+      }, animationDuration);
     } else {
-      ref.current.classList.add('header_button_cross');
+      classList.remove('header_button-active');
+      classList.add('header_button_cross');
+      setTimeout(() => {
+        classList.add('header_button_cross-active');
+      }, animationDuration);
     }
     setIsClicked(!isClicked);
     callback();
   };
 
   return (
-    <div className="header_button" onClick={onClick} ref={ref}>
+    <div className="header_button header_button-active" onClick={onClick} ref={ref}>
       <i className="header_button_line first_line"></i>
       <i className="header_button_line second_line"></i>
       <i className="header_button_line third_line"></i>
