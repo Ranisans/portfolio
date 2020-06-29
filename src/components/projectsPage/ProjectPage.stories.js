d95f56380ac321c4f60e0d476320cc35d03f1257
Ranisans <ranisans@yahoo.com>
2020-06-29 16:23:53 +0300

    feat: add project page

---
+++ src/components/projectsPage/ProjectPage.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import ProjectPage from './ProjectsPage';
+
+export default {
+  title: 'Project Page',
+  component: ProjectPage,
+};
+
+export const Base = () => <ProjectPage />;
---
+++ src/components/projectsPage/ProjectsPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { projects } from './projects.json';
+import ProjectCard from '../projects/ProjectCard';
+
+const ProjectPage = () => (
+  <div className="page projects_page">
+    <h2 className="projects_title">web developer portfolio</h2>
+    <ul className="projects">
+      {projects.map((project) => <ProjectCard
+        image={project.image}
+        label={project.label}
+        description={project.description}
+        technologies={project.technologies}
+        projectName={project.projectName}
+        link={project.link}
+        key={project.projectName}
+      />)}
+    </ul>
+  </div>
+);
+
+export default ProjectPage;
