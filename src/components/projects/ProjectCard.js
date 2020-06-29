c7e9e0052d061736cc912497645830f11ea2324c
Ranisans <ranisans@yahoo.com>
2020-06-29 15:13:58 +0300

    feat: add project laber element

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -5,10 +5,20 @@ import ProjectPicture from './ProjectPicture';
 import Tag from './Tag';
 
 const ProjectCard = ({
-  image, description, technologies, projectName, link,
+  image, label, description, technologies, projectName, link,
 }) => (
     <li className="projects_item" key={projectName}>
       <a href={link} className="project_screen">
+        {
+          label
+            ? <div
+              className="project_label"
+              style={{ backgroundColor: label.color }}
+            >
+              {label.text}
+            </div>
+            : null
+        }
         <ProjectBar projectName={projectName} />
         <div className="project_data_container">
           <div className="project_description_container">
--- src/components/projects/ProjectCard.stories.js
+++ src/components/projects/ProjectCard.stories.js
@@ -8,12 +8,22 @@ export default {
 };
 
 const image = 'piskel_clone.jpeg';
-const description = 'Light clone of piskelapp.com - free online editor for animated sprites & pixel art';
+const description = 'Simple clone of piskelapp.com - free online editor for animated sprites & pixel art';
 const technologies = ['html5', 'scss', 'react', 'redux', 'canvas'];
 const projectName = 'Simple Piskel Clone';
 const link = 'https://ranisans.github.io/simple-piskel-clone/';
+const label = { text: 'chrome', color: 'rgb(173, 110, 110)' };
 
 export const Base = () => <ProjectCard
+  image={image}
+  label={label}
+  description={description}
+  technologies={technologies}
+  projectName={projectName}
+  link={link}
+/>;
+
+export const WithoutLabel = () => <ProjectCard
   image={image}
   description={description}
   technologies={technologies}
