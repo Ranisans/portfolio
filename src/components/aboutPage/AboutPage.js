7417cba025b17681f285891dadb3bbdf8c1a8d58
Ranisans <ranisans@yahoo.com>
2020-06-30 14:02:46 +0300

    feat: about page set to respnsive remove Contact menu from header menu

--- src/components/aboutPage/AboutPage.js
+++ src/components/aboutPage/AboutPage.js
@@ -5,8 +5,9 @@ import data from './about.json';
 const AboutPage = () => (
   <div className="page page_about">
     <div className="about_container">
-      <p className="about_greeting">
-        {data.title} <span className="about_name">{data.name}</span></p>
+      <div className="about_greeting">
+        <span>{data.title}</span>
+        <span className="about_name">{data.name}</span></div>
       <p className="about_representation">{data.representation}</p>
     </div>
   </div>
