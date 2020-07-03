2fbea8379ba4f12fb354d37ae6e3ce30041ce95a
Ranisans <ranisans@yahoo.com>
2020-07-03 15:54:37 +0300

    feat: collect all pages in the app with router add style to scrollbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+
+import HeaderAssembly from './components/header/HeaderAssembly';
+import AboutPage from './components/aboutPage/AboutPage';
+import ProjectsPage from './components/projectsPage/ProjectsPage';
+
+import './assets/scss/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <HeaderAssembly />
+      <Switch>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/portfolio">
+          <ProjectsPage />
+        </Route>
+        <Route path="/">
+          <AboutPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
