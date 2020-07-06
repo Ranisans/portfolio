cb67d287cbdbb85789391491fca6b624643580a4
Rani <ranisans@yahoo.com>
2020-07-06 20:53:25 +0300

    feat: create client-side page

---
+++ precache-manifest.aec6c4e91bb782c138c8821e73e058a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5f2516523872f2675abc26515c4b60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55369b4461e86803d9e2",
+    "url": "./static/css/main.9c8ea6af.chunk.css"
+  },
+  {
+    "revision": "e6cbea509070951f89b6",
+    "url": "./static/js/2.8fe3343b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8fe3343b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55369b4461e86803d9e2",
+    "url": "./static/js/main.8e7e0288.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "5ab286e40fbc0cd04eebc304d6fac018",
+    "url": "./static/media/belarus_writers.5ab286e4.jpeg"
+  },
+  {
+    "revision": "70c9aa9eb92188a95e3b0d223a63d729",
+    "url": "./static/media/fancy_weather.70c9aa9e.jpeg"
+  },
+  {
+    "revision": "d90d7ff216befb8fc9102e27ea6fc9b2",
+    "url": "./static/media/loan_lease_calculator.d90d7ff2.jpeg"
+  },
+  {
+    "revision": "9d22f97553c67519fa9fdfbcbc3e5777",
+    "url": "./static/media/piskel_clone.9d22f975.jpeg"
+  },
+  {
+    "revision": "47808fad54e590a1d7c47ed14fa78317",
+    "url": "./static/media/repair_design.47808fad.jpeg"
+  }
+]);
